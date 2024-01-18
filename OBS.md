72366227ddd6c98de5a722874cd9dd3bb23bd2ba
luizefz <luizefsilva2@gmail.com>
2024-01-18 18:35:49 -0300

    Feat: Ajuste funcao monte_carlos, elaboracao da ideia principal

---
+++ OBS.md
@@ -0,0 +1,9 @@
+# Ideia principal 👨‍💻
+
+A ideia agora é utilizar Monte Carlo com rd.choices para calcular a possibilidade de uma peça de dominó está na mão de um dos jogores. 
+
+# Processo de advinhação 🔮
+
+1. Ao longo das rodadas do jogo, com as peças jogadas, cada jogador vai construindo o seu próprio dicionário com as vezes que cada peça / número da peça foi jogado.
+
+2. Quando for realizar o sorteio com o método monte carlo, ele usará o dicionário de cada jogador com seus respectivos pesos, visando sortear uma estimativa mais correta da quantidade de vezes que a peça pode aparecer na mão do jogador.
--- calc_probalidade.py
+++ calc_probalidade.py
@@ -1,26 +1,5 @@
 import random as rd
 
-def resultado_probaliddade_num(pecas_disponiveis):   
-    return rd.sample(pecas_disponiveis, 4)
-        
-
-def verifica_probabilidade(i, resultado_sorteio) -> bool:
-    if i in resultado_sorteio:
-        return True
-    return
-    
-def verifica_peca_sorteio(pecas_disponiveis: dict, domino_dict):
-    for i in pecas_disponiveis:
-        resultado_sorteio = resultado_probaliddade_num(pecas_disponiveis)
-        verificaca = verifica_probabilidade(i, resultado_sorteio)
-        
-        if verificaca == True:
-            domino_dict[i] += 1
-            
-    return print(f'dicionario com o valores que apareceram no sortei:\n{domino_dict},\nResultado do sorteio:\n{resultado_sorteio}')
-            
-            
-            
 dicionario_teste = {(0, 0):0, (0, 1):0, (0, 2):0, (0, 3):0, (0, 4):0, (0, 5):0, (0, 6):0,
                  (1, 1):0, (1, 2):0, (1, 3):0, (1, 4):0, (1, 5):0, (1, 6):0,
                  (2, 2):0, (2, 3):0, (2, 4):0, (2, 5):0, (2, 6):0,
@@ -30,24 +9,48 @@ dicionario_teste = {(0, 0):0, (0, 1):0, (0, 2):0, (0, 3):0, (0, 4):0, (0, 5):0, 
                  (6, 6):0
                  }
 
-print(verifica_peca_sorteio([(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6),
-                 (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6),
-                 (2, 2), (2, 3), (2, 4), (2, 5), (2, 6),
-                 (3, 3), (3, 4), (3, 5), (3, 6),
-                 (4, 4), (4, 5), (4, 6),
-                 (5, 5), (5, 6),
-                 (6, 6)], dicionario_teste))
+dicionario_pecas_peso = {(0, 0):0, (0, 1):0, (0, 2):0, (0, 3):0, (0, 4):0, (0, 5):0, (0, 6):0,
+                 (1, 1):0, (1, 2):0, (1, 3):0, (1, 4):0, (1, 5):0, (1, 6):0,
+                 (2, 2):0, (2, 3):0, (2, 4):0, (2, 5):0, (2, 6):0,
+                 (3, 3):0, (3, 4):0, (3, 5):0, (3, 6):0,
+                 (4, 4):0, (4, 5):0, (4, 6):0,
+                 (5, 5):0, (5, 6):0,
+                 (6, 6):0
+                 }
 
+
+def pegar_pecas_aleatorias_dic(pecas_disponiveis, quant_elementos_aleatorios): 
+    numeros_aleatorios = rd.sample(pecas_disponiveis, quant_elementos_aleatorios)
+    return numeros_aleatorios
+
+def verifica_pecas_alea_dispo(peca_aleatoria, pecas_disponiveis):
+    if peca_aleatoria in pecas_disponiveis:
+        return True
+    return
     
     
-
-
-
-print(resultado_probaliddade_num([(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6),
-                 (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6),
-                 (2, 2), (2, 3), (2, 4), (2, 5), (2, 6),
-                 (3, 3), (3, 4), (3, 5), (3, 6),
-                 (4, 4), (4, 5), (4, 6),
-                 (5, 5), (5, 6),
-                 (6, 6)]))
-
+def verifica_peca_sorteio(pecas_disponiveis: dict, domino_dict, quant_elementos_aleatorios):
+    
+    resultado_sorteio = pegar_pecas_aleatorias_dic(pecas_disponiveis, quant_elementos_aleatorios)
+    
+    for i in resultado_sorteio:
+        verificaca = verifica_pecas_alea_dispo(i, pecas_disponiveis)
+        
+        if verificaca == True:
+            domino_dict[i] += 1
+            
+    return domino_dict
+            
+            
+for i in range(100):
+    s = verifica_peca_sorteio([(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6),
+                                            (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6),
+                                            (2, 2), (2, 3), (2, 4), (2, 5), (2, 6),
+                                            (3, 3), (3, 4), (3, 5), (3, 6),
+                                            (4, 4), (4, 5), (4, 6),
+                                            (5, 5), (5, 6),
+                                            (6, 6)], dicionario_teste, 4)
+    domino_teste = s
+    
+domino_teste = sorted(domino_teste.items(), key=lambda x: x[1], reverse=True)
+print(f'\n{domino_teste}')
